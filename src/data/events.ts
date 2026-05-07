5d841c7350a1201d155d740d57319178169fc5a0
MrBrain314 <ourokingbastoss@gmail.com>
2026-05-07 23:36:00 +0200

    Mise en page de la partie Events

---
+++ src/data/events.ts
@@ -0,0 +1,26 @@
+export const events = [
+  {
+    id: 1,
+    day: "28",
+    month: "Feb, 2022",
+    time: "10:30am To 2:30pm",
+    place: "Poland",
+    title: "Business creativity workshops",
+  },
+  {
+    id: 2,
+    day: "15",
+    month: "Mar, 2022",
+    time: "10:30am To 2:30pm",
+    place: "Poland",
+    title: "Street Performance: Call for Art.",
+  },
+  {
+    id: 3,
+    day: "20",
+    month: "May, 2022",
+    time: "10:30am To 2:30pm",
+    place: "Poland",
+    title: "Digital transformation conference",
+  },
+];
